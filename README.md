88cd1e3481209bdcd5c3afaac2257841a259346d
luczNV <nvrrlucas@gmail.com>
2024-12-09 23:30:28 -0300

    actualizacion

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # santex-aquario
+Es necesario instalar las siguientes librerias para el funcionamiento correcto:
+"pip install django-allauth"
+"pip install crispy-bootstrap5"
+"pip install django-crispy-forms"
+
--- apli/urls.py
+++ apli/urls.py
@@ -3,7 +3,14 @@ from . import views
 urlpatterns = [
     path('',views.home, name='inicio'),
     path('productos/',views.producto,name='producto'),
-    path('carrito/', views.carrito, name='carrito'),
-    path('register/',views.register, name='register'),
+    path('productos/<int:producto_id>/', views.producto_views, name='producto_views'),
+    path('checkout/',views.checkout, name='checkout'),
     path('logout/',views.logout_view, name='logout'),
+    path('carrito/', views.Carrito, name='carrito'),
+    path('agregar-carrito/<int:producto_id>/', views.agregar_carrito, name='agregar_carrito'),
+    path('cantidad-carrito/', views.global_carrito_cantidad, name='global_carrito_cantidad'),
+    path('carrito/bajar/<int:item_id>/', views.bajar_cantidad, name='bajar_cantidad'),
+    path('carrito/aumentar/<int:item_id>/', views.aumentar_cantidad, name='aumentar_cantidad'),
+    path('carrito/eliminar/<int:item_id>/', views.eliminar_item, name='eliminar_item'),
+    path('gracias/', views.gracias, name='gracias'),
 ]
